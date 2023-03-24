2aacabd18d7414f477ad695997c00c323645104f
Victor Feliciano <victorjfeliciano@gmail.com>
2023-03-24 17:28:03 -0300

    add eventlistener to the Header component so the change in style is applied

--- src/componentes/Header/Header.jsx
+++ src/componentes/Header/Header.jsx
@@ -1,7 +1,27 @@
-import './Header.css'
+import './Header.css';
 import ProgrammerIcon from '../../assets/icons/header-icon.png';
+import { useEffect } from 'react';
 
 export default function Header() {
+    useEffect(() => {
+        const header = document.querySelector('.Header');
+        const headerBborder = header.style.borderBottom;
+
+        document.addEventListener('scroll', () => {
+            if (window.scrollY > 0) {
+                header.style.backgroundImage = 'linear-gradient(to bottom right, #001848, #301860)';
+                //header.style.borderBottom = 'none';
+            } else {
+                header.style.backgroundImage = 'none';
+                header.style.borderBottom = headerBborder;
+            }
+        });
+
+        return () => {
+            document.removeEventListener('scroll');
+        };
+    }, []);
+
     return (
         <header className="Header">
             <img src={ProgrammerIcon} alt="ícone programador" />
