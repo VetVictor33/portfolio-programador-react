114a0319f266fefb876a9f82ab87ff9c2e249e4e
Victor Feliciano <victorjfeliciano@gmail.com>
2023-03-24 17:52:49 -0300

    add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,15 @@
 import './App.css'
 import Header from './componentes/Header/Header'
-import Presentation from './componentes/Presentation/Presentation'
-import SocialNavbar from './componentes/SocialNavbar/SocialNavbar'
-import Projects from './componentes/Projects/Projects'
-/*
-import Footer from './componentes/Footer'*/
+import Footer from './componentes/Footer/Footer'
+import Home from './componentes/Home/Home'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
-      <Presentation />
-      <SocialNavbar />
-      <Projects />
-      {/*
-  <Footer />*/}
+      <Home />
+      <Footer />
     </div>
   )
 }
---
+++ src/componentes/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+export default function Footer() {
+    return (
+        <footer className='Footer'>
+            <spam>Victor Feliciano - ©2023</spam>
+        </footer>
+    )
+}
