e6ac641cb712f8a0e7d6ef413f0bcc9ead8f0039
Victor Feliciano <victorjfeliciano@gmail.com>
2023-03-25 18:03:45 -0300

    changed svg color to match the page pattern

--- src/componentes/ProjectPage/ProjectPage.jsx
+++ src/componentes/ProjectPage/ProjectPage.jsx
@@ -8,11 +8,13 @@ export default function ProjectPage() {
     const projectIndex = params.project;
     const project = projectsMock[projectIndex - 1];
 
-    const nextProject = project.id >= ((projectsMock.length) - 1) ? 1 : (project.id) + 1;
+    const nextProject = project.id >= (projectsMock.length) ? 1 : (project.id) + 1;
     const preveiusProject = project.id != 1 ? (project.id) - 1 : projectsMock.length;
     return (
         <div className='ProjectPage'>
-            <img src={`.${project.imgSrc}`} alt="projeto" />
+            <a href={project.link} >
+                <img src={`.${project.imgSrc}`} alt="projeto" />
+            </a>
             <div className='bts'>
                 <Link className='bt bt-left' to={`/project/${preveiusProject}`}></Link>
                 <Link className='bt bt-right' to={`/project/${nextProject}`}></Link>
