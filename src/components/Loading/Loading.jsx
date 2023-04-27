84f2ac48909925fcb9dd58c798917cec2eeb65cc
Victor Feliciano <victorjfeliciano@gmail.com>
2023-04-27 17:24:10 -0300

    issue fixed, new request if no data

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,17 +1,34 @@
 import { useEffect, useState } from 'react'
 import './Loading.css'
+import { getProjectsFromLocalStorage, requestProjects } from '../../services/database/repository';
 
-export default function Loading() {
+export default function Loading({ setProjects }) {
     const [timeOut, setTimeOut] = useState(false);
 
-    setTimeout(() => setTimeOut(true), 13000);
+    async function getProjectsFromApi() {
+        const projects = await requestProjects();
+        setProjects(projects);
+        console.log('setei os projects')
+    }
+
+    setTimeout(() => {
+        if (!getProjectsFromLocalStorage()) {
+            getProjectsFromApi();
+        }
+    }, 1500);
+
+    setTimeout(() => {
+        setTimeOut(true);
+    }, 10000);
+
+    useEffect(() => { }, [])
     return (
         <div className="Loading">
             <h1>Carregando...</h1>
             <div className='bar-container'>
                 <div className='bar' />
             </div>
-            {timeOut && <strong className='apologies'>Sinto muito... parece que houve um erro ao conectar com o banco de dados. Aguarde um pouco ou volte mais tarde</strong>}
+            {timeOut && <strong className='apologies'>Sinto muito... parece que houve um erro ao conectar com o banco de dados. <a href='/'>Recarregue a página</a> ou volte mais tarde</strong>}
         </div>
     )
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -46,7 +46,7 @@ export default function Projects() {
                         )
                     })
                     :
-                    <Loading />
+                    <Loading setProjects={setProjects} />
                 }
             </div>
         </section>
