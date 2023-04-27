4925ff17f522bdbe3bbfc12a0e5891a7a255d122
Victor Feliciano <victorjfeliciano@gmail.com>
2023-04-27 08:37:46 -0300

    improving Loading component style

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,12 +1,17 @@
+import { useEffect, useState } from 'react'
 import './Loading.css'
 
 export default function Loading() {
+    const [timeOut, setTimeOut] = useState(false);
+
+    setTimeout(() => setTimeOut(true), 13000);
     return (
         <div className="Loading">
             <h1>Carregando...</h1>
             <div className='bar-container'>
                 <div className='bar' />
             </div>
+            {timeOut && <strong className='apologies'>Sinto muito... parece que houve um erro ao conectar com o banco de dados. Aguarde um pouco ou volte mais tarde</strong>}
         </div>
     )
 }
