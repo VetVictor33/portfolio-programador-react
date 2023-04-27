cb78b9c4a511b402169d4817d953f43802821aa6
Victor Feliciano <victorjfeliciano@gmail.com>
2023-04-27 10:04:57 -0300

    corrented misspelling

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -29,13 +29,13 @@ export default function Projects() {
                 <h1 className="main-title">Projetos Desenvolvidos</h1>
                 <img className="icon projects-img" src={ProjectImg} />
                 <p className="text-description">
-                    Aqui estão alguns projetos pessoais e outros propostos em aulas. São projetos Backend, Frontend e FullSatck. Alguns têm responsividade
+                    Aqui estão alguns projetos pessoais e outros propostos em aulas. São projetos <span className='texto-destaque'>Backend, Frontend e FullSatck</span>. Alguns têm responsividade
                     para dar suporte a diversos tamanhos de tela, incluindo dispositivos mobile.
                 </p >
                 <p className="text-description">Clique em uns dos cards abaixo para  navegar entre os projetos e encontrar mais detalhes,
                     incluindo o link do repositório no GitHub e o link do deploy, quando presente!
                 </p>
-                <p className="text-description"> A principais tecnologias são JavaScript, React, Node, HTML e CSS.
+                <p className="text-description"> As principais tecnologias são <span className='texto-destaque'>JavaScript, React, Node, HTML e CSS</span>.
                 </p>
             </article>
             <div className="projects-cards">
