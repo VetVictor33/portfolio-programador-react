1d23c6a3d28dc25156f90f766a2dda72123e3319
Victor Feliciano <victorjfeliciano@gmail.com>
2023-08-28 14:01:03 -0300

    removed wite spaces

---
+++ src/components/Footer/Footer.test.jsx
@@ -0,0 +1,19 @@
+import { render, screen } from "@testing-library/react"
+import Footer from "./Footer"
+
+describe("Footer", () => {
+  it("should render name", () => {
+    const name = /Victor Feliciano/i
+
+    render(<Footer />)
+
+    expect(screen.getByText(name)).toBeInTheDocument()
+  })
+  it("should render current year", () => {
+    const year = new RegExp(new Date().getFullYear())
+
+    render(<Footer />)
+
+    expect(screen.getByText(year)).toBeInTheDocument()
+  })
+})
