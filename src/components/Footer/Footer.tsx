4ca320ecae618b2fa52eea7b32b49e70f3102889
Victor Feliciano <115307935+VetVictor33@users.noreply.github.com>
2024-02-27 07:25:25 -0300

    feat: update footer date to 2024

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import './Footer.sass'
 export default function Footer() {
     return (
         <footer className='Footer'>
-            <span>Victor Feliciano - ©2023</span>
+            <span>Victor Feliciano - ©2024</span>
         </footer>
     )
 }
